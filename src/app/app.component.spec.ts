291bc91f36b33d365d6b8a12920b9bdb0264ba28
Colton Hurst <colton@coltonhurst.com>
2021-08-25 16:56:52 -0400

    Fix failing test and remove unnecessary test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hex-notes-web'`, () => {
+  it(`should have as title 'Hex Notes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hex-notes-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hex-notes-web app is running!');
+    expect(app.title).toEqual('Hex Notes');
   });
 });
